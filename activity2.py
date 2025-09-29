fa4aa046bffd992fe0e2764712410b3798231a7f
MonoGutsy <monogutsy69@gmail.com>
2025-09-29 21:58:45 +0800

    Add files via upload

---
+++ activity2.py
@@ -0,0 +1,17 @@
+name = input("Enter you name:   ")
+age = eval(input("Enter your age:   "))
+email = input("What is you email:   ")
+print("Hi" , name , " , " , age , " yrs old\n","Email:   " , email)
+
+print("Your name has " , len(name) , "characters")
+
+num1 = eval(input("Please enter a number:   "))
+num2 = eval(input("Please enter another number: "))
+
+print(type(num1))
+print(type(num2))
+print(num1 , " + " , num2 , " = " , num1 + num2)
+
+
+#COMMENTS
+#THIS THE INTRODUCTION OF COMMENTS
---
+++ activity3.py
@@ -0,0 +1,6 @@
+#String Formatting
+
+name = input("Enter you name:   ")
+age = eval(input("Enter your age:   "))
+add = input("What is your address:   ")
+print(f"Hi! {name} from {add} age of {age}")
